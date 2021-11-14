e8e4d96c9a78411fdb4fa9e7c082bf890e484494
Pierre7Agus <redtor_90@outlook.es>
2021-11-14 08:19:40 -0500

    Add state Add state for the recipe object and add a state to modify the prepared value that belongs to the ingredients property. A useEffect has been added to run every time the initial state is changed

--- Project-1/src/App.jsx
+++ Project-1/src/App.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
+import './style/style.css'
 import recipe from './services/Data'
 import Header from './components/Header'
 import ListOfIngredients from './components/ListOfIngredients';
 
+
 export default function App(){
+
+    const [initial, setInitial] = useState(recipe);
+    const [prepared , setPrepared] = useState(false);
+
+    function ingredientClick(index){
+        const update = {... initial}
+
+        update.ingredients[index].prepared = !update.ingredients[index].prepared
+
+        setInitial(update)
+    }
+
+    useEffect(()=>{
+        setPrepared(recipe.ingredients.every(i => i.prepared))
+    },[initial]);
+
     return(
         <div>
         <Header titulo={recipe.title} descripcion={recipe.description}/>
-        <ListOfIngredients ingredients={recipe.ingredients}/>
+        <ListOfIngredients ingredients={recipe.ingredients} onClick={ingredientClick} prepared={prepared}/>
+        
         </div>
     )
 }
--- Project-1/src/components/ListOfIngredients.jsx
+++ Project-1/src/components/ListOfIngredients.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 
-function ListOfIngredients({ingredients}){
+function ListOfIngredients({ingredients, onClick,prepared}){
 
     const ingredientes = ingredients.map((item,index)=>{
         return(
-            <li key={index}>{item.name}</li>
+            <li key={index} onClick={()=>onClick(index)} className={(item.prepared) ? 'prepared':''}>{item.name}</li>
         )
     })
 
     return(
         <section>
             <h2>Lista de Ingredientes</h2>
+            <p>Marca los ingredientes que tengas listos.</p>
             <ul>
                 {ingredientes}
             </ul>
+            {prepared ? <h3>Ya tienes todos los ingredientes necesarios. ¡Comencemos!</h3> : <h3>Aún no tienes todos los ingredientes</h3> }
         </section>
     )
 }
