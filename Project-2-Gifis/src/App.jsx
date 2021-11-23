6274d1b37b3620f5c971b3b1ba349991afddd69a
Pierre7Agus <redtor_90@outlook.es>
2021-11-23 11:40:45 -0500

    Add getGifis.jsx service Add the getGifis function that allows obtaining the information from the API and exporting this function by default in order to import it from the main component

--- Project-2-Gifis/src/App.jsx
+++ Project-2-Gifis/src/App.jsx
@@ -1,25 +1,9 @@
 import React,{useState,useEffect} from 'react'
+import getGifis from './services/getGifis'
 
 export default function App(){
 
   const [gif , setGif]=useState([])
-
-  const apiKEY = '3gsGIyDZ1ugFQ8tzmLXKONspga919v28'
-  const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKEY}&q='panda'&limit=10&offset=0&rating=g&lang=en`;
-
-  const getGifis=()=>{
-    return fetch(apiUrl)
-    .then(response=>response.json())
-    .then(res=>{
-      const {data}=res
-      const gifs=data.map((item)=>{
-        const {title,id,images}=item
-        const {url}=images.downsized_medium
-        return {title,id,url}
-      })
-      return gifs
-    })
-  }
 
   useEffect(()=>{
     getGifis().then(res=>setGif(res))
---
+++ Project-2-Gifis/src/services/getGifis.jsx
@@ -0,0 +1,18 @@
+const apiKEY = '3gsGIyDZ1ugFQ8tzmLXKONspga919v28'
+
+export default function getGifis(){
+
+  const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKEY}&q='panda'&limit=10&offset=0&rating=g&lang=en`;
+
+  return fetch(apiUrl)
+  .then(response=>response.json())
+  .then(res=>{
+    const {data}=res
+    const gifs=data.map((item)=>{
+      const {title,id,images}=item
+      const {url}=images.downsized_medium
+      return {title,id,url}
+    })
+    return gifs
+  })
+}
